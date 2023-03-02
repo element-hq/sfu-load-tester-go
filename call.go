6fbdea1e7a75f19b9d4c72812f14c310f7992b80
Daniel Abramov <daniel.abramov@element.io>
2023-03-02 17:07:32 +0100

    call: handle rate-limitting

--- call.go
+++ call.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/playwright-community/playwright-go"
@@ -19,7 +20,7 @@ func newChromium() (*chromium, error) {
 	}
 
 	launchOptions := playwright.BrowserTypeLaunchOptions{
-		Headless: playwright.Bool(true),
+		Headless: playwright.Bool(false),
 		Args: []string{
 			"--no-sandbox",
 			"--use-fake-ui-for-media-stream",
@@ -43,27 +44,39 @@ func newChromium() (*chromium, error) {
 // Runs the bots until the stop signal is received.
 // Uses the array of supplied bots as the bots to run.
 func (c *chromium) runBots(callURL string, bots []string, ctx context.Context) error {
-	pages := make([]playwright.Page, len(bots))
-
 	go func() {
 		<-ctx.Done()
+		for _, browserCtx := range c.browser.Contexts() {
+			browserCtx.Close()
+		}
 		c.browser.Close()
 	}()
 
-	for i, bot := range bots {
+	for _, bot := range bots {
 		page, err := c.spawnBot(callURL, bot)
+
 		if err != nil {
-			return fmt.Errorf("could not spawn bot: %v", err)
+			if !strings.Contains(err.Error(), "Timeout") {
+				return err
+			}
+
+			// It's likely a rate-limitting issue, so we'll just wait a bit and try again.
+			fmt.Printf("Hitting rate-limit, waiting to bypass the rate-limitter...:\n")
+
+			select {
+			case <-ctx.Done():
+			case <-time.After(3 * time.Second):
+			}
+
+			// Try again, if we failed again, then it's not rate-limitting, but something else.
+			page, err = c.spawnBot(callURL, bot)
+			if err != nil {
+				return err
+			}
 		}
 
-		pages[i] = page
+		fmt.Printf("Bot %s spawned\n", bot)
 		defer page.Close()
-
-		// Sleep to bypass the rate-limiting.
-		select {
-		case <-time.After(3 * time.Second):
-		case <-ctx.Done():
-		}
 	}
 
 	// Wait for the stop signal.
@@ -78,33 +91,42 @@ func (c *chromium) spawnBot(callURL string, bot string) (playwright.Page, error)
 		return nil, fmt.Errorf("could not create context: %v", err)
 	}
 
+	// Fail relatively early (the default value is 30s).
+	context.SetDefaultTimeout(5000)
+
+	// Convenient function to handle Go boilerplate.
+	returnError := func(err error, text string) (playwright.Page, error) {
+		defer context.Close()
+		return nil, fmt.Errorf("%s: %v", text, err)
+	}
+
 	page, err := context.NewPage()
 	if err != nil {
-		return nil, fmt.Errorf("could not create page: %v", err)
+		return returnError(err, "could not create page")
 	}
 
 	if _, err := page.Goto(callURL); err != nil {
-		return nil, fmt.Errorf("could not goto page: %v", err)
+		return returnError(err, "could not go to call URL")
 	}
 
 	if err := page.Click("text='Log in'"); err != nil {
-		return nil, fmt.Errorf("could not click login: %v", err)
+		return returnError(err, "could not click login button")
 	}
 
 	if err := page.Fill("[placeholder='Username']", bot); err != nil {
-		return nil, fmt.Errorf("could not fill username: %v", err)
+		return returnError(err, "could not fill username")
 	}
 
 	if err := page.Fill("input[placeholder=Password]", bot); err != nil {
-		return nil, fmt.Errorf("could not fill password: %v", err)
+		return returnError(err, "could not fill password")
 	}
 
 	if err := page.Click("text=Login"); err != nil {
-		return nil, fmt.Errorf("could not click login: %v", err)
+		return returnError(err, "could not click login button")
 	}
 
 	if err := page.Click("text='Join call now'"); err != nil {
-		return nil, fmt.Errorf("could not press enter: %v", err)
+		return returnError(err, "could not click join call button")
 	}
 
 	return page, nil
